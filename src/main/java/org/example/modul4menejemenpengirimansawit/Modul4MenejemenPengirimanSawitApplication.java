5eb865a80a72519e30842c5972a222b6f5a9468c
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2026-02-19 13:25:29 +0700

    set up menejemen pengiriman sawit

---
+++ src/main/java/org/example/modul4menejemenpengirimansawit/Modul4MenejemenPengirimanSawitApplication.java
@@ -0,0 +1,13 @@
+package org.example.modul4menejemenpengirimansawit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modul4MenejemenPengirimanSawitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modul4MenejemenPengirimanSawitApplication.class, args);
+    }
+
+}
