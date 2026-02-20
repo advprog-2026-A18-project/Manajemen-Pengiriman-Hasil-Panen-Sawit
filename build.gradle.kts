b3724240bfc9b1321ade3af380d2634bf800a58f
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2026-02-20 19:17:01 +0700

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.sonarqube") version "5.0.0.4638"
+    id("org.sonarqube") version "7.2.0.6526"
 }
 
 group = "org.example"
